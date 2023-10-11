910a41e354e4ea04d8ed369fe1fe2a14d4d7455b
Artem Chernitsa <chernicaar@gmail.com>
2023-10-11 20:18:02 +0300

    init solution

---
+++ Numerical/Program.cs
@@ -0,0 +1,23 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+
+            ScottPlot.Plot myPlot = new(400, 300);
+            myPlot.AddScatter(dataX, dataY);
+
+            new ScottPlot.FormsPlotViewer(myPlot).ShowDialog();
+        }
+    }
+}
