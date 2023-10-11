23631e37744bad567ca6203aad284d31afb0f528
Artem Chernitsa <chernicaar@gmail.com>
2023-10-12 00:09:20 +0300

    Implemented Vector and Grid classes. Define interfaces for numerical methods.

--- Numerical/Program.cs
+++ Numerical/Program.cs
@@ -1,9 +1,5 @@
 ﻿using ScottPlot;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Numerical
 {
@@ -11,13 +7,23 @@ namespace Numerical
     {
         static void Main(string[] args)
         {
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 9, 16, 25 };
+            //double[] dataX = { 1, 2, 3, 4, 5 };
+            //double[] dataY = { 1, 4, 9, 16, 25 };
 
-            ScottPlot.Plot myPlot = new(400, 300);
-            myPlot.AddScatter(dataX, dataY);
+            //ScottPlot.Plot myPlot = new(400, 300);
+            //myPlot.AddScatter(dataX, dataY);
 
-            new ScottPlot.FormsPlotViewer(myPlot).ShowDialog();
+            //new ScottPlot.FormsPlotViewer(myPlot).ShowDialog();
+            Vector x = new(new double[]{ 1.0, 2.0, 3.0});
+            Vector y = Vector.Copy(x);
+            Console.WriteLine(x == y);
+            Console.WriteLine(object.ReferenceEquals(x, y));
+            Console.WriteLine(x != y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+
+            Console.ReadKey();
         }
     }
 }
